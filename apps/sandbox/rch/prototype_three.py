53cf7f7704a4586552398eb06063d01e5a4172a3
rch <rostislav.chudoba@rwth-aachen.de>
2016-05-03 15:43:29 +0200

    scaffolding export for prototype 3

--- apps/sandbox/rch/prototype_three.py
+++ apps/sandbox/rch/prototype_three.py
@@ -6,7 +6,7 @@ Created on Jan 20, 2016
 
 from traits.api import \
     Float, HasTraits, Property, cached_property, Int, \
-    Instance
+    Instance, Array
 
 import numpy as np
 from oricreate.api import MappingTask
@@ -194,31 +194,106 @@ class DoublyCurvedYoshiFormingProcess(HasTraits):
         return SimulationTask(previous_task=self.init_displ_task,
                               config=sim_config, n_steps=self.n_steps)
 
-    def generate_scaffolding(self):
+    def generate_scaffolding(self, x_scaff_position):
+
         ft = self.fold_task
         cp = ft.formed_object
         x_1 = ft.x_1
         L = cp.L
-        x_L = x_1[L]
-        xx_L = x_L[:, :, 0]
-        print 'x_L', x_L.shape
-        x_L_ordered = np.array([np.min(xx_L, axis=1), np.max(xx_L, axis=1)])
-        x_m = self.L_x / 2.0
-        print x_L_ordered.shape
-        L_mid_idx = np.where(
-            (x_L_ordered[0, :] < x_m) & (x_L_ordered[1, :] > x_m))
-        print 'line indexes', L_mid_idx
-        x_L_cut_idx = cp.L[L_mid_idx[0]]
-        x_L_cut = x_1[x_L_cut_idx]
-        print 'line end nodes', x_L_cut.shape
-        x_values = x_L_cut[:, :, 1].flatten()
-        y_values = x_L_cut[:, :, 2].flatten()
-        print x_values
-        print 'y'
-        print y_values
+
+        p_0 = np.array([x_scaff_position, 0, 0], dtype='float_')
+        l_0 = x_1[L[:, 0]]
+        l = cp.L_vectors
+        n = np.array([1.0, 0, 0], dtype='float_')
+
+        p_0_l_0 = p_0[np.newaxis, :] - l_0
+        nom = np.einsum('...i,...i->...', p_0_l_0, n[np.newaxis, :])
+        denom = np.einsum('...i,...i->...', l, n[np.newaxis, :])
+        d = nom / denom
+
+        l_idx = np.where((d < 1.001) & (d > -0.001))[0]
+        p = d[l_idx, np.newaxis] * l[l_idx, :] + l_0[l_idx, :]
+
+        x_values, y_values = p[:, (1, 2)].T
+        six = np.argsort(x_values)
+        sx_values, sy_values = x_values[six], y_values[six]
+        spoints = np.c_[sx_values, sy_values]
+        left_points, right_points = spoints[:-1, :], spoints[1:, :]
+        svects = right_points - left_points
+        norm_svects = np.linalg.norm(svects, axis=1)
+        l_idx = np.where(norm_svects > 0.01)[0]
+        svects[l_idx]
+        print norm_svects[l_idx]
+
+        last_x = sx_values[l_idx[-1] + 1]
+        last_y = sy_values[l_idx[-1] + 1]
+        x, y = sx_values[l_idx], sy_values[l_idx]
+
+#        print 'last', last_x, last_y
+
+        return np.hstack([x, [last_x]]), np.hstack([y, [last_y]])
+
+    scaff_positions = Array
+
+    def _scaff_positions_default(self):
+        pos = np.array(
+            [-1.065, -0.77, -0.45, -0.25, 0.25, 0.45, 0.77, 1.065], dtype='float_')
+        return pos
+
+    def generate_scaffoldings(self):
+
+        L_mid = 1.5
+        pos = self.scaff_positions
+        middle = (pos[-1] + pos[0]) / 2.0
+        offset = L_mid - middle
+        centered_pos = pos + offset
+
+        scaff_plates = []
+        min_max = []
+        for s_pos in centered_pos:
+            x, y = self.generate_scaffolding(s_pos)
+            min_max.append([np.min(x), np.max(x), np.min(y), np.max(y)])
+            scaff_plates.append(np.array([x, y]))
+
+        min_max_arr = np.array(min_max)
+        min_x = np.min(min_max_arr[:, 0])
+        max_x = np.max(min_max_arr[:, 1])
+        min_y = np.min(min_max_arr[:, 2])
+#        max_y = np.max(min_max_arr[:, 3])
+        min_scaff_height = 0.05
+
+        x_0 = (min_x + max_x) / 2.0
+        y_0 = min_y + min_scaff_height
+
+        scaff_plates_0 = [[x - x_0, y - y_0] for x, y in scaff_plates]
+
+        import tempfile
+        import os.path
         import pylab as p
-        ax = p.axes()
-        ax.plot(x_values, y_values)
+
+        tdir = tempfile.mkdtemp()
+        for idx, (x_y, s_pos) in enumerate(zip(scaff_plates_0, self.scaff_positions)):
+            x, y = x_y
+            x_close = [np.max(x), np.min(x)]
+            y_close = [0, 0]
+
+            p.clf()
+            x_p = np.hstack([x, x_close, [x[0]]])
+            y_p = np.hstack([y, y_close, [y[0]]])
+
+            ax = p.axes()
+            ax.axis('equal')
+
+            ax.plot(x_p, y_p)
+            for x_v, y_v in zip(x, y):
+                ax.annotate('%5.3f,%5.3f' %
+                            (x_v, y_v), xy=(x_v, y_v), rotation=90)
+            ax.annotate('scaffold x - position %5.3f' % s_pos, xy=(0, 0.04))
+
+            fname_path = os.path.join(tdir, 'scaff%d.pdf' % idx)
+            print 'saving in %s', fname_path
+            p.savefig(fname_path)
+
         p.show()
 
 
@@ -263,7 +338,7 @@ if __name__ == '__main__':
     it.u_1
     ft.u_1
 
-    bsf_process.generate_scaffolding()
+    bsf_process.generate_scaffoldings()
 
     ftv.plot()
     ftv.update(vot=1, force=True)
