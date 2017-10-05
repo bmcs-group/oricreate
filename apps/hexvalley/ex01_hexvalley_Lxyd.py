33a98052c609b2e42ed895a1a1e5d7f37f0e697f
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2017-10-05 21:35:36 +0200

    parameterized waterbomb

--- apps/hexvalley/ex01_hexvalley_Lxyd.py
+++ apps/hexvalley/ex01_hexvalley_Lxyd.py
@@ -4,6 +4,8 @@ Created on Jan 20, 2016
 @author: rch
 '''
 
+from math import sqrt
+
 from traits.api import \
     Float, HasStrictTraits, Property, cached_property, Int, \
     Instance, Array, Bool, List
@@ -15,7 +17,6 @@ from oricreate.api import YoshimuraCPFactory, \
     GuConstantLength, GuDofConstraints, \
     GuPsiConstraints, SimulationConfig, SimulationTask, \
     FTV, FTA
-from oricreate.crease_pattern.crease_pattern_state import CreasePatternState
 from oricreate.export import \
     InfoCadMeshExporter, ScaffoldingExporter
 from oricreate.forming_tasks.forming_task import FormingTask
@@ -44,6 +45,8 @@ class HexYoshiFormingProcess(HasStrictTraits):
 
     n_cell_x = Int(1, auto_set=False, enter_set=True, input=True)
     n_cell_y = Int(1, auto_set=False, enter_set=True, input=True)
+    xi = Float(1.0, auto_set=False, enter_set=True, input=True)
+    h = Float(0.0, auto_set=False, enter_set=True, input=True)
 
     n_x = Property
     '''Number of cross cells in x direction
@@ -65,16 +68,30 @@ class HexYoshiFormingProcess(HasStrictTraits):
     '''control target surface'''
     @cached_property
     def _get_ctf(self):
-        h = 0.2
-        return [r_, s_, h * t_ * s_ * (1 - s_ / self.L_y) - h * t_]
+        h = self.h
+        return [r_, s_, h * t_ * s_ * (1 - s_ / float(self.L_y * self.n_y)) - h * t_]
 
     factory_task = Property(Instance(FormingTask))
     '''Factory task generating the crease pattern.
     '''
     @cached_property
     def _get_factory_task(self):
-        return YoshimuraCPFactory(L_x=self.L_x, L_y=self.L_y,
-                                  n_x=self.n_x, n_y=self.n_y)
+        yf = YoshimuraCPFactory(L_x=self.L_x * self.n_cell_x,
+                                L_y=self.L_y * self.n_cell_y,
+                                n_x=self.n_x, n_y=self.n_y)
+        cp = yf.formed_object
+        N_h = yf.N_h
+        N_i = yf.N_i
+
+        dx = self.L_x / 3 * (1.0 - self.xi)
+
+        print '***shortest length', self.L_x / 2.0 - dx
+
+        cp.X[N_h[1::3, :].flatten(), 0] -= dx
+        cp.X[N_h[2::3, :].flatten(), 0] += dx
+        cp.X[N_i[0::3, :].flatten(), 0] += dx
+        cp.X[N_i[2::3, :].flatten(), 0] -= dx
+        return yf
 
     init_displ_task = Property(Instance(FormingTask))
     '''Initialization to render the desired folding branch. 
@@ -85,7 +102,7 @@ class HexYoshiFormingProcess(HasStrictTraits):
         return MapToSurface(previous_task=self.factory_task,
                             target_faces=[(self.ctf, cp.N)])
 
-    psi_max = Float(-np.pi * 0.468)
+    psi_max = Float(-np.pi * 0.45)
 
     fold_angle_cntl = Property(Instance(FormingTask))
     '''Configure the simulation task.
@@ -121,14 +138,12 @@ class HexYoshiFormingProcess(HasStrictTraits):
         base_h_y = corner2_h_y
 
         for c_x in range(0, self.n_cell_x):
-            print 'c_x', c_x
             base_i_x = np.hstack([base_i_x, 3 * c_x + tb2_i_x])
             base_i_y = np.hstack([base_i_y, tb2_i_y])
             base_h_x = np.hstack([base_h_x, 3 * c_x + tb2_h_x])
             base_h_y = np.hstack([base_h_y, tb2_h_y])
 
         for c_x in range(0, self.n_cell_x - 1):
-            print 'c_x', c_x
             base_i_x = np.hstack([base_i_x, 3 * c_x + right2_i_x])
             base_i_y = np.hstack([base_i_y, right2_i_y])
             base_h_x = np.hstack([base_h_x, 3 * c_x + right2_h_x])
@@ -192,10 +207,13 @@ class HexYoshiFormingProcess(HasStrictTraits):
                           N_i[2::3, :].flatten(),
                           N_v[:, :].flatten()])
         print 'N_up', N_up
-        cp.u[N_down, 2] -= .1
-        cp.u[N_up, 2] += .1
+        cp.u[N_down, 2] -= self.d_down
+        cp.u[N_up, 2] += self.d_up
 
         return st
+
+    d_up = Float(0.01)
+    d_down = Float(0.01)
 
 
 class HexYoshiFormingProcessFTV(FTV):
@@ -204,10 +222,47 @@ class HexYoshiFormingProcessFTV(FTV):
 
 
 if __name__ == '__main__':
-    bsf_process = HexYoshiFormingProcess(L_x=16, L_y=40,
-                                         n_cell_x=4, n_cell_y=4,
-                                         n_fold_steps=10,
-                                         n_load_steps=1)
+    kw1 = dict(L_x=8,
+               L_y=6,
+               d_x=0.52,
+               h=0.001, d_up=0.2, d_down=0.2,
+               psi_max=-np.pi * 0.52,
+               n_cell_x=4, n_cell_y=3,
+               n_fold_steps=10,
+               n_load_steps=1)
+    kw2 = dict(L_x=0.6,
+               L_y=1.1,
+               xi=0.67,
+               h=0.005, d_up=0.001, d_down=0.001,
+               psi_max=-np.pi * 0.52,
+               n_cell_x=1, n_cell_y=2,
+               n_fold_steps=20,
+               n_load_steps=1)
+    kw3 = dict(L_x=6,
+               L_y=14,
+               d_x=0.4,
+               h=0.2, d_up=0.001, d_down=0.001,
+               psi_max=-np.pi * 0.52,
+               n_cell_x=3, n_cell_y=2,
+               n_fold_steps=20,
+               n_load_steps=1)
+    kw4 = dict(L_x=6.0,
+               L_y=3.0 * sqrt(6.0**2 / 2.),
+               d_x=00,
+               h=0.2, d_up=0.001, d_down=0.3,
+               psi_max=-np.pi * 0.345,
+               n_cell_x=3, n_cell_y=4,
+               n_fold_steps=30,
+               n_load_steps=1)
+    kw5 = dict(L_x=0.6,
+               L_y=1.1,
+               xi=0.5,
+               h=0.005, d_up=0.001, d_down=0.001,
+               psi_max=-np.pi * 0.52,
+               n_cell_x=1, n_cell_y=2,
+               n_fold_steps=20,
+               n_load_steps=1)
+    bsf_process = HexYoshiFormingProcess(**kw5)
 
     ftv = HexYoshiFormingProcessFTV(model=bsf_process)
 
--- apps/hexvalley/ex01_pwaterbomb_ahc.py
+++ apps/hexvalley/ex01_pwaterbomb_ahc.py
@@ -31,22 +31,30 @@ import sympy as sp
 a_, b_ = sp.symbols('a,b')
 
 
-class HexYoshiFormingProcess(HasStrictTraits):
+class WBShellFormingProcess(HasStrictTraits):
     '''
     Define the simulation task prescribing the boundary conditions, 
     target surfaces and configuration of the algorithm itself.
     '''
 
-    L_x = Float(3.0, auto_set=False, enter_set=True, input=True)
-    L_y = Float(2.2, auto_set=False, enter_set=True, input=True)
-    u_max = Float(0.1, auto_set=False, enter_set=True, input=True)
+    L_x = Property
+
+    def _get_L_x(self):
+        return (self.a + 2 * self.c) * self.n_cell_x
+    L_y = Property
+
+    def _get_L_y(self):
+        return self.h * 2 * self.n_cell_y
+
     n_fold_steps = Int(30, auto_set=False, enter_set=True, input=True)
     n_load_steps = Int(30, auto_set=False, enter_set=True, input=True)
 
     n_cell_x = Int(1, auto_set=False, enter_set=True, input=True)
     n_cell_y = Int(1, auto_set=False, enter_set=True, input=True)
-    xi = Float(1.0, auto_set=False, enter_set=True, input=True)
-    h = Float(0.0, auto_set=False, enter_set=True, input=True)
+    d_r = Float(0.1, auto_set=False, enter_set=True, input=True)
+    a = Float(1.0, auto_set=False, enter_set=True, input=True)
+    h = Float(1.0, auto_set=False, enter_set=True, input=True)
+    c = Float(1.0, auto_set=False, enter_set=True, input=True)
 
     n_x = Property
     '''Number of cross cells in x direction
@@ -68,29 +76,31 @@ class HexYoshiFormingProcess(HasStrictTraits):
     '''control target surface'''
     @cached_property
     def _get_ctf(self):
-        h = self.h
-        return [r_, s_, h * t_ * s_ * (1 - s_ / float(self.L_y * self.n_y)) - h * t_]
+        d_r = self.d_r
+        return [r_, s_,
+                d_r * t_ * s_ * (1 - s_ / float(self.L_y)) - d_r * t_]
 
     factory_task = Property(Instance(FormingTask))
     '''Factory task generating the crease pattern.
     '''
     @cached_property
     def _get_factory_task(self):
-        yf = YoshimuraCPFactory(L_x=self.L_x * self.n_cell_x,
-                                L_y=self.L_y * self.n_cell_y,
-                                n_x=self.n_x, n_y=self.n_y)
+        yf = YoshimuraCPFactory(L_x=self.L_x,
+                                L_y=self.L_y,
+                                n_x=self.n_x,
+                                n_y=self.n_y)
         cp = yf.formed_object
         N_h = yf.N_h
         N_i = yf.N_i
 
-        dx = self.L_x / 3 * (1.0 - self.xi)
-
-        print '***shortest length', self.L_x / 2.0 - dx
-
-        cp.X[N_h[1::3, :].flatten(), 0] -= dx
-        cp.X[N_h[2::3, :].flatten(), 0] += dx
-        cp.X[N_i[0::3, :].flatten(), 0] += dx
-        cp.X[N_i[2::3, :].flatten(), 0] -= dx
+        e = (self.a + 2 * self.c) / 3.0
+        print 'e', e
+        d_x = e - self.c
+        print 'delta', d_x
+        cp.X[N_h[1::3, :].flatten(), 0] -= d_x
+        cp.X[N_h[2::3, :].flatten(), 0] += d_x
+        cp.X[N_i[0::3, :].flatten(), 0] += d_x
+        cp.X[N_i[2::3, :].flatten(), 0] -= d_x
         return yf
 
     init_displ_task = Property(Instance(FormingTask))
@@ -104,9 +114,6 @@ class HexYoshiFormingProcess(HasStrictTraits):
 
     psi_max = Float(-np.pi * 0.45)
 
-    fold_angle_cntl = Property(Instance(FormingTask))
-    '''Configure the simulation task.
-    '''
     @cached_property
     def _get_fold_angle_cntl(self):
 
@@ -175,16 +182,127 @@ class HexYoshiFormingProcess(HasStrictTraits):
         fixed_nodes_z = fix(
             [N_h[0, 0], N_h[-1, 0], N_h[0, -1]], (2))
 
-        dof_constraints = fixed_nodes_x + fixed_nodes_z + fixed_nodes_y
+        u_max = 1.999 * self.c
+        link_mid = link(
+            N_i[0, 0], (0), 1.0,
+            N_i[2, 0], (0), -1.0,
+            lambda t: t * u_max
+        )
+
+        print '--------------------------'
+        print N_i[0, 0].flatten()
+        print N_i[2, 0].flatten()
+        print '--------------------------'
+
+        dof_constraints = fixed_nodes_x + fixed_nodes_z + fixed_nodes_y + \
+            link_mid
+
+        gu_dof_constraints = GuDofConstraints(dof_constraints=dof_constraints)
+
+        def FN(psi): return lambda t: psi * t
+        psi_constr = [([(psi_lines[0], 1.0)], FN(self.psi_max))]
+        lpsi_constr = [([(psi_lines[0], 1.0), (i, -1.0)], 0.0)
+                       for i in psi_lines[1:]]
+
+        gu_psi_constraints = \
+            GuPsiConstraints(forming_task=self.init_displ_task,
+                             psi_constraints=lpsi_constr)
+
+        gu_constant_length = GuConstantLength()
+        sim_config = SimulationConfig(goal_function_type='none',
+                                      gu={'cl': gu_constant_length,
+                                          'dofs': gu_dof_constraints,
+                                          'psi': gu_psi_constraints},
+                                      acc=1e-5, MAX_ITER=500,
+                                      debug_level=0)
+
+        st = SimulationTask(previous_task=self.init_displ_task,
+                            config=sim_config, n_steps=self.n_fold_steps)
+
+        cp = st.formed_object
+
+        N_down = np.hstack([N_h[::3, :].flatten(),
+                            N_i[1::3, :].flatten()
+                            ])
+        print 'N_down', N_down
+        N_up = np.hstack([N_i[::3, :].flatten(),
+                          N_i[2::3, :].flatten(),
+                          N_v[:, :].flatten()])
+        print 'N_up', N_up
+        cp.u[N_down, 2] -= self.d_down
+        cp.u[N_up, 2] += self.d_up
+
+        return st
+
+    fold_angle_cntl = Property(Instance(FormingTask))
+    '''Configure the simulation task.
+    '''
+    @cached_property
+    def x_get_fold_angle_cntl(self):
+
+        self.init_displ_task.x_1
+
+        corner_i_x = np.array([0, -1], dtype=np.int_)
+        corner_i_y = np.array([0, 0], dtype=np.int_)
+        corner_h_x = np.array([1, -2], dtype=np.int_)
+        corner_h_y = np.array([0, 0], dtype=np.int_)
+
+        ref_i_x = 0
+        ref_i_y = 0
+        ref_h_x = 0
+        ref_h_y = 0
+
+        link_i_x = np.array([0, 2, 2], dtype=np.int_)
+        link_i_y = np.array([-1, -1, 0], dtype=np.int_)
+        link_h_x = np.array([0, 3, 3], dtype=np.int_)
+        link_h_y = np.array([-1, -1, 0], dtype=np.int_)
+
+        f = self.factory_task
+        cp = f.formed_object
+        m_nodes = f.N_i[corner_i_x, corner_i_y]
+        n_nodes = f.N_h[corner_h_x, corner_h_y]
+
+        psi_lines = cp.NN_L[m_nodes, n_nodes].flatten()
+
+        print 'psi_lines', psi_lines
+
+        rm_nodes = f.N_i[ref_i_x, ref_i_y]
+        rn_nodes = f.N_h[ref_h_x, ref_h_y]
+        rpsi_line = cp.NN_L[rm_nodes, rn_nodes].flatten()
+
+        print 'rpsi_lines', rpsi_line
+
+        lm_nodes = f.N_i[link_i_x, link_i_y]
+        ln_nodes = f.N_h[link_h_x, link_h_y]
+        lpsi_lines = cp.NN_L[lm_nodes, ln_nodes].flatten()
+
+        print 'lpsi_lines', lpsi_lines
+
+        N_h = f.N_h
+        N_i = f.N_i
+        N_v = f.N_v
+        fixed_nodes_x = fix(
+            N_h[0, 0], (0))
+        fixed_nodes_y = fix(
+            N_h[(0, -1), 0], (1))
+        fixed_nodes_z = fix(
+            [N_h[0, 0], N_h[-1, 0], N_h[0, -1]], (2))
+
+        link_nodes = link(N_h[1, (0, -1)], 2, 1.0, N_h[2, (0, -1)], 2, -1.0)
+
+        dof_constraints = fixed_nodes_x + fixed_nodes_z + fixed_nodes_y + \
+            link_nodes
 
         gu_dof_constraints = GuDofConstraints(dof_constraints=dof_constraints)
 
         def FN(psi): return lambda t: psi * t
         psi_constr = [([(i, 1.0)], FN(self.psi_max))
                       for i in psi_lines]
+        lpsi_constr = [([(rpsi_line, 1.0), (i, -1.0)], 0.0)
+                       for i in lpsi_lines]
         gu_psi_constraints = \
             GuPsiConstraints(forming_task=self.init_displ_task,
-                             psi_constraints=psi_constr)
+                             psi_constraints=psi_constr + lpsi_constr)
 
         gu_constant_length = GuConstantLength()
         sim_config = SimulationConfig(goal_function_type='none',
@@ -216,47 +334,40 @@ class HexYoshiFormingProcess(HasStrictTraits):
     d_down = Float(0.01)
 
 
-class HexYoshiFormingProcessFTV(FTV):
+class WBShellFormingProcessFTV(FTV):
 
-    model = Instance(HexYoshiFormingProcess)
+    model = Instance(WBShellFormingProcess)
 
 
 if __name__ == '__main__':
-    kw1 = dict(L_x=8,
-               L_y=6,
-               d_x=0.52,
-               h=0.001, d_up=0.2, d_down=0.2,
-               psi_max=-np.pi * 0.52,
-               n_cell_x=4, n_cell_y=3,
-               n_fold_steps=10,
-               n_load_steps=1)
-    kw2 = dict(L_x=0.6,
-               L_y=1.1,
-               xi=0.67,
-               h=0.005, d_up=0.001, d_down=0.001,
+    kw1 = dict(a=0.36,
+               c=0.13,
+               h=0.55,
+               d_r=0.001, d_up=0.05, d_down=0.05,
                psi_max=-np.pi * 0.52,
                n_cell_x=1, n_cell_y=2,
                n_fold_steps=20,
                n_load_steps=1)
-    kw3 = dict(L_x=6,
-               L_y=14,
-               d_x=0.4,
-               h=0.2, d_up=0.001, d_down=0.001,
-               psi_max=-np.pi * 0.52,
-               n_cell_x=3, n_cell_y=2,
+    kw2 = dict(a=0.6,
+               h=0.8,
+               c=0.6,
+               d_r=0.001, d_up=0.05, d_down=0.05,
+               psi_max=-np.pi * 0.8,
+               n_cell_x=2, n_cell_y=3,
                n_fold_steps=20,
                n_load_steps=1)
-    kw4 = dict(L_x=6.0,
-               L_y=3.0 * sqrt(6.0**2 / 2.),
-               d_x=00,
-               h=0.2, d_up=0.001, d_down=0.3,
-               psi_max=-np.pi * 0.345,
-               n_cell_x=3, n_cell_y=4,
-               n_fold_steps=30,
+    kw3 = dict(a=0.6,
+               h=0.8,
+               c=0.3,
+               d_r=0.0001, d_up=0.005, d_down=0.005,
+               psi_max=-np.pi * 0.8,
+               n_cell_x=1, n_cell_y=1,
+               n_fold_steps=20,
                n_load_steps=1)
-    bsf_process = HexYoshiFormingProcess(**kw2)
 
-    ftv = HexYoshiFormingProcessFTV(model=bsf_process)
+    bsf_process = WBShellFormingProcess(**kw1)
+
+    ftv = WBShellFormingProcessFTV(model=bsf_process)
 
     fa = bsf_process.factory_task
 
